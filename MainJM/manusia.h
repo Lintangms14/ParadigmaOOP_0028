134f3f6d3feea63e14d14c31540aed9ab10fc896
Lintangms14 <merayusukmalintang@gmail.com>
2023-05-24 21:09:19 +0700

    class manusia

---
+++ MainJM/manusia.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia {
+public:
+	string nama;
+	jantung varJantung;
+
+	manusia(string pName) :nama(pName) {
+		cout << nama << "hidup\n";
+	}
+	~manusia() {
+		cout << nama << "mati\n";
+	}
+};
+#endif
